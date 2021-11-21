0b91b05d074ab5e36cb5abf38c67f366c52fbc22
bianguojie <bianguojie@rockontrol.com>
2021-11-21 20:53:19 +0800

    init project

---
+++ basic/hello.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+"""
+@File: hello.py
+@Project: basic
+@Desc: hello universe
+@Time: 2021/11/21 20:30:47
+@Author: wuwenrufeng (wuwenrufeng@163.com)
+@Last Modified: 2021/11/21 20:44:41
+@Modified By: wuwenrufeng (wuwenrufeng@163.com)
+@Version: 1.0
+@License: Copyright(C) 2021 - 2021 wuwenrufeng, Borland
+"""
+
+print("Hello,Universe!")
---
+++ basic/num_cal.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+"""
+@File: num_cal.py
+@Project: basic
+@Desc: 常见算术运算
+@Time: 2021/11/21 20:45:54
+@Author: wuwenrufeng (wuwenrufeng@163.com)
+@Last Modified: 2021/11/21 20:45:57
+@Modified By: wuwenrufeng (wuwenrufeng@163.com)
+@Version: 1.0
+@License: Copyright(C) 2021 - 2021 wuwenrufeng, Borland
+"""
+
+x, y = 3, 2
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x // y)
+print(x % y)
+print(-x)
+print(abs(-x))
+print(int(3.9))
+print(float(x))
+print(x ** y)
+
+# result:
+# 5
+# 1
+# 6
+# 1.5
+# 1
+# 1
+# -3
+# 3
+# 3
+# 3.0
+# 9
