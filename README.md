ca5e567479dac1cacae837493b9f506373f146b7
bianguojie <bianguojie@rockontrol.com>
2021-11-28 21:30:03 +0800

    feat: add declaration

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # python-review-codes
 python-review-codes
+个人复习笔记
+By:[菜鸟教程](https://www.runoob.com/python3/python3-tutorial.html) 
---
+++ basic/6_type.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+"""
+@File: 6_type.py
+@Project: basic
+@Desc: 类型
+@Time: 2021/11/28 21:25:03
+@Author: wuwenrufeng (wuwenrufeng@163.com)
+@Last Modified: 2021/11/28 21:25:06
+@Modified By: wuwenrufeng (wuwenrufeng@163.com)
+@Version: 1.0
+@License: Copyright(C) 2021 - 2021 wuwenrufeng, Borland
+"""
+
+# String
+word = '字符串'
+sentence = "之二是一个"
